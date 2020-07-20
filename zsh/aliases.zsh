c26b6e0dc2ea93d688f9e25bcbf366b16a65e691
Ryan Jadhav <ryan.jadhav@gmail.com>
2020-07-20 11:55:20 -0700

    cleanup

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,12 +1,6 @@
 alias reload!='. ~/.zshrc'
 
 alias work='cd ~/Work'
-alias lyft-api='cd ~/Work/instant-server'
-alias lyft-com='cd ~/Work/lyft.com'
-alias brochure='cd ~/Work/brochure'
-alias enterprisefrontend='cd ~/Work/enterprisefrontend'
-
-alias sync-enterprisefrontend="cd ~/Work/enterprisefrontend && ~/Work/hacktools/sync-to-onebox-v3.sh enterprisefrontend-legacy-rjadhav"
-alias sync-tool="~/Work/hacktools/sync-to-onebox-v3.sh"
+alias sync-to-onebox="~/Work/hacktools/sync-to-onebox-v3.sh"
 
 alias curbr='git status | head -1 | awk '{print $3}' | pbcopy'
