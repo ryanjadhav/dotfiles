b5d3944bd0b2ab2f2cbbd658668a523d09df6a2c
Ryan Jadhav <ryan.jadhav@gmail.com>
2020-07-21 15:30:55 -0700

    add hyper

--- zsh/awsaccess.zsh
+++ zsh/awsaccess.zsh
@@ -1,3 +1,3 @@
-source '/Users/rjadhav/Work/awsaccess/awsaccess2.sh' # awsaccess
-export PS1="\$(ps1_mfa_context)$PS1" # awsaccess
+#source '/Users/rjadhav/Work/awsaccess/awsaccess2.sh' # awsaccess
+#export PS1="\$(ps1_mfa_context)$PS1" # awsaccess
 
